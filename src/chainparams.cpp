feaa9ad17432773788e53586c454a846ad1a4f7d
HafeezRai <rai.hafeez006@gmail.com>
2020-08-28 06:04:00 +0100

    Test block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -243,9 +243,14 @@ public:
         networkID = CBaseChainParams::TESTNET;
         strNetworkID = "test";
 
-        genesis = CreateGenesisBlock(1577169143, 268438, 1, 1, 250 * COIN);
+        genesis = CreateGenesisBlock(1577168939, 2647271, 0x1e0ffff0, 1, 1 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0xbceb9bd1247969b008e4545f2219048e5948b5e0a6fd166288ba5685b6e98d73"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000e2dc2b1db786db6578d77c738ae9efabccecffc45f772046c885bced630"));
+        assert(genesis.hashMerkleRoot == uint256S("0xd0928a9f84607dfd323b5ad288861d658a866f91bb39c3b57de72514ece925a2"));
+
+        // genesis = CreateGenesisBlock(1577169143, 268438, 1, 1, 250 * COIN);
+        // consensus.hashGenesisBlock = genesis.GetHash();
+        // assert(consensus.hashGenesisBlock == uint256S("0xbceb9bd1247969b008e4545f2219048e5948b5e0a6fd166288ba5685b6e98d73"));
     //  assert(genesis.hashMerkleRoot == uint256S("0x1b2ef6e2f28be914103a277377ae7729dcd125dfeb8bf97bd5964ba72b6dc39b"));
 
         consensus.fPowAllowMinDifficultyBlocks = true;
@@ -361,9 +366,14 @@ public:
         networkID = CBaseChainParams::REGTEST;
         strNetworkID = "regtest";
 
-        genesis = CreateGenesisBlock(1577169143, 268438, 1, 1, 250 * COIN);
+        genesis = CreateGenesisBlock(1577168939, 2647271, 0x1e0ffff0, 1, 1 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0xbceb9bd1247969b008e4545f2219048e5948b5e0a6fd166288ba5685b6e98d73"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000e2dc2b1db786db6578d77c738ae9efabccecffc45f772046c885bced630"));
+        assert(genesis.hashMerkleRoot == uint256S("0xd0928a9f84607dfd323b5ad288861d658a866f91bb39c3b57de72514ece925a2"));
+
+        // genesis = CreateGenesisBlock(1577169143, 268438, 1, 1, 250 * COIN);
+        // consensus.hashGenesisBlock = genesis.GetHash();
+        // assert(consensus.hashGenesisBlock == uint256S("0xbceb9bd1247969b008e4545f2219048e5948b5e0a6fd166288ba5685b6e98d73"));
     //  assert(genesis.hashMerkleRoot == uint256S("0x1b2ef6e2f28be914103a277377ae7729dcd125dfeb8bf97bd5964ba72b6dc39b"));
 
         consensus.fPowAllowMinDifficultyBlocks = true;
